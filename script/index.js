99fdd73c8c5a209a20f59a4506466556091c0eb3
oldmanstreetcoding <oldmanstreetcoding@gmail.com>
2021-07-23 00:33:24 +0700

    menambahkan jumbotron dan rak buku

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+/* eslint-disable linebreak-style */
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('hello world');
+  document.getElementById('hero-search').focus();
+
+  const btnAddBook = document.getElementById('btn-add-book');
+  const boxInput = document.getElementById('box-modal');
+
+  btnAddBook.addEventListener('click', () => {
+    boxInput.style.display = 'block';
+  });
 });
